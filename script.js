f6ce915e723ff05dc1141be18f4a4d38acb59890
Nithishkumars-2001 <nithishkumar70102@gmail.com>
2025-04-02 16:00:30 +0530

    file added

---
+++ script.js
@@ -0,0 +1,11 @@
+//select sidenavbar
+var sidenavbar = document.querySelector(".side-navbar")
+
+function shownavbar()
+{
+    sidenavbar.style.left = "0"
+}
+function closenavbar()
+{
+    sidenavbar.style.left = "-60%"
+}
